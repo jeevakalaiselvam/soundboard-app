5191ce598b193dc727c988eee0b5334a4f62d129
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-29 20:03:23 +0530

    sounds added

---
+++ script.js
@@ -0,0 +1,14 @@
+const boards = document.getElementById("boards");
+
+const sounds = [
+    "avengersiphone.mp3",
+    "bollywood.mp3",
+    "buttabomma.mp3",
+    "jacksparrow.mp3",
+    "johncenawwe.mp3",
+    "jokerbgm.mp3",
+    "kgfbgm.mp3",
+    "pikachu.mp3",
+    "suicidesquadbgm.mp3",
+    "takitakiiphone.mp3",
+];
